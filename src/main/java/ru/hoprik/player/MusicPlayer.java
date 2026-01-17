54787d98d0d3f0b0ef0d1c28d80a11bbfbe4f183
hoprik V.M Morozov <hoprik@hoprik.ru>
2026-01-17 17:05:57 +0300

    стой dexik

--- src/main/java/ru/hoprik/player/MusicPlayer.java
+++ src/main/java/ru/hoprik/player/MusicPlayer.java
@@ -1,8 +1,5 @@
 package ru.hoprik.player;
 
-import org.telegram.ui.ActionBar.BaseFragment;
-import ru.hoprik.player.ui.MusicPlayerUI;
-
 public class MusicPlayer {
     private static MusicPlayer instance = new MusicPlayer();
 
